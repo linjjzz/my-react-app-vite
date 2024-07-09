53568520ad950abf740588d5ef57dc4be5bdb0ae
linjjzz <1322558575@qq.com>
2024-07-09 17:01:12 +0800

    feat: 保存

--- src/pages/question/Edit/EditHeader.tsx
+++ src/pages/question/Edit/EditHeader.tsx
@@ -1,8 +1,11 @@
+import { editQuestionService } from '@/services/request'
+import { useComponentInfoStore } from '@/store/useComponentInfoStore'
 import { usePageInfoStore } from '@/store/usePageInfoStore'
 import { DownOutlined, LeftOutlined } from '@ant-design/icons'
-import { Button, Space, Typography } from 'antd'
+import { useDebounceEffect, useKeyPress, useRequest } from 'ahooks'
+import { Button, Space, Typography, message } from 'antd'
 import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import EditToolbar from './EditToolbar'
 
 const { Title } = Typography
@@ -10,6 +13,40 @@ const { Title } = Typography
 function EditHeader() {
   const navigator = useNavigate()
   const { pageInfo, resetPageInfo } = usePageInfoStore()
+  const { componentList } = useComponentInfoStore()
+  const { id = '' } = useParams()
+
+  const { loading: saveLoading, run: saveQuestion } = useRequest(
+    async () => {
+      if (!id) return
+      await editQuestionService(id, { ...pageInfo, componentList })
+    },
+    { manual: true },
+  )
+
+  const { loading: publishLoading, run: publishQuestion } = useRequest(
+    async () => {
+      if (!id) return
+      // await editQuestionService(id, { ...pageInfo, componentList })
+    },
+    { manual: true },
+  )
+
+  useKeyPress(['ctrl.s', 'meta.s'], (e: KeyboardEvent) => {
+    e.preventDefault()
+    if (saveLoading) return
+    saveQuestion()
+    message.success('保存成功')
+  })
+
+  // 自动保存
+  useDebounceEffect(
+    () => {
+      saveQuestion()
+    },
+    [pageInfo, componentList],
+    { wait: 3000 },
+  )
 
   const editTitle = (title: string) => {
     resetPageInfo({ ...pageInfo, title })
@@ -38,8 +75,20 @@ function EditHeader() {
       </div>
       <div>
         <Space>
-          <Button icon={<DownOutlined />}>保存</Button>
-          <Button type="primary">发布</Button>
+          <Button
+            loading={saveLoading}
+            icon={<DownOutlined />}
+            onClick={saveQuestion}
+          >
+            保存
+          </Button>
+          <Button
+            loading={saveLoading}
+            type="primary"
+            onClick={publishQuestion}
+          >
+            发布
+          </Button>
         </Space>
       </div>
     </>
